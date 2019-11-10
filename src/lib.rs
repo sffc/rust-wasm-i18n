432aad6fe545c8b1e68ae23a26f485f20cc950de
Shane F. Carr <shane.carr@wustl.edu>
2019-11-09 17:51:21 -0800

    Minor experiments.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+extern crate wee_alloc;
+
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -11,9 +13,14 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
+    fn alert(d: &f64);
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, rust-wasm-i18n-2!");
+pub fn greet(name: &str) {
+	// let mut message = String::new();
+	// message.push_str("Hello, ");
+	// message.push_str(name);
+	// alert(&message);
+	alert(3.14);
 }
