f8f185002cef8b7b711bfcb163e5d6f9f4991fdc
Shane F. Carr <shane@unicode.org>
2020-05-15 18:02:55 -0500

    Building with FEATURES=std and wee_alloc

--- wasm-no-std/src/types/std.rs
+++ wasm-no-std/src/types/std.rs
@@ -7,6 +7,20 @@ use ruststd::error::Error;
 
 use crate::intl::IntlError;
 
+/////////////////////////////////
+// BEGIN WEE_ALLOC BOILERPLATE //
+/////////////////////////////////
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// NOTE: Can't override panic_handler or alloc_error_handler in a no_std crate.
+
+///////////////////////////////
+// END WEE_ALLOC BOILERPLATE //
+///////////////////////////////
+
 impl fmt::Display for IntlError {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		write!(f, "IntlError")
